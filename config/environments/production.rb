97466752af64601635e4029d0f5fbf5133d94a49
Jones Agyemang <mightyj@hotmail.co.uk>
2017-01-04 17:32:21 +0000

    sets smtp config in prod only if available

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,7 +16,7 @@ Rails.application.configure do
   config.log_tags = [ :request_id ]
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = SMTP_SETTINGS
+  config.action_mailer.smtp_settings = SMTP_SETTINGS if ENV['SMTP_ADDRESS'].present?
   config.i18n.fallbacks = true
   config.active_support.deprecation = :notify
   config.log_formatter = ::Logger::Formatter.new
