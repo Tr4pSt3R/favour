5b2324a3fe4fe6c6093af17d4ee78fcc99da2b57
Jones Agyemang <mightyj@hotmail.co.uk>
2017-01-04 17:27:46 +0000

    removes mandatory requirement of SMTP creds
      * we have no use it for it
      * it causes the build to fail

--- config/smtp.rb
+++ config/smtp.rb
@@ -1,12 +1,14 @@
-SMTP_SETTINGS = {
-  address: ENV.fetch("SMTP_ADDRESS"), # example: "smtp.sendgrid.net"
-  authentication: :plain,
-  domain: ENV.fetch("SMTP_DOMAIN"), # example: "heroku.com"
-  enable_starttls_auto: true,
-  password: ENV.fetch("SMTP_PASSWORD"),
-  port: "587",
-  user_name: ENV.fetch("SMTP_USERNAME")
-}
+if ENV['SMTP_ADDRESS'].present?
+  SMTP_SETTINGS = {
+    address: ENV.fetch("SMTP_ADDRESS"), # example: "smtp.sendgrid.net"
+    authentication: :plain,
+    domain: ENV.fetch("SMTP_DOMAIN"), # example: "heroku.com"
+    enable_starttls_auto: true,
+    password: ENV.fetch("SMTP_PASSWORD"),
+    port: "587",
+    user_name: ENV.fetch("SMTP_USERNAME")
+  }
+end
 
 if ENV["EMAIL_RECIPIENTS"].present?
   Mail.register_interceptor RecipientInterceptor.new(ENV["EMAIL_RECIPIENTS"])
